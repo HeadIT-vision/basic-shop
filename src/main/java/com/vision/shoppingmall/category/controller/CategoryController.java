9d6e7e4274097901be0917bd88a6ec13eca43f7c
Uuuug <songyuji30@naver.com>
2024-12-05 22:59:47 +0900

    -

--- src/main/java/com/vision/shoppingmall/category/controller/CategoryController.java
+++ src/main/java/com/vision/shoppingmall/category/controller/CategoryController.java
@@ -1,6 +1,7 @@
 package com.vision.shoppingmall.category.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -11,6 +12,12 @@ public class CategoryController {
   @GetMapping("")
   public String getCategoryList() {
     return "category/list";
+  }
+
+  @GetMapping("/new-category")
+  public String createCategoryForm(Model model) {
+    model.addAttribute("modalTitle", "카테고리 등록하기");
+    return "category/category-form";
   }
 
 }
---
+++ src/main/java/com/vision/shoppingmall/category/model/entity/Category.java
@@ -0,0 +1,8 @@
+package com.vision.shoppingmall.category.model.entity;
+
+import jakarta.persistence.Entity;
+
+@Entity(name = "categories")
+public class Category {
+
+}
